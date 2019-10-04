1fda47356e530ad0f3a11d373bf73668a1ac2734
JonnhyMarlison <renat.enokaev@mail.ru>
2019-10-04 20:52:36 +0300

    lab4-6 completed.

--- lab5.cpp
+++ lab5.cpp
@@ -4,25 +4,12 @@
 using namespace std;
 
 int main(){
-	float x, y, line_x1, line_x2, line_y1, line_y2;
+	float x, y;
 
 	cout << "Enter x: " << endl;
 	cin >> x;
 	cout << "Enter y: " << endl;
 	cin >> y;
-
-	line_x1 = (2 * y - 4) / 5; 
-	line_x2 = (-2 * y + 4) / 5;
-
-	line_y1 = -2.5 * x + 2;
-	line_y2 = 2.5 * x + 2;
-
-
-	/*
-	y = -3
-	y = 2.5x+2
-	y = -2.5x+2
-	*/
 
 	if (((-2 <= 2.5 * x - y) && (2 >= 2.5 * x + y )) && (y >= -3) && (y <= 2)){
 		cout << "This point in triangle." << endl;
--- lab6.cpp
+++ lab6.cpp
@@ -6,8 +6,8 @@ using std::cin;
 using std::endl;
 
 int main(){
-    int x1, y1, x2, y2, r1, r2;
-    float distanse;
+    int x1, y1, x2, y2;
+    float distance, r1, r2, sum_radius;
 
     cout << "Enter coordinates of the first circle: " << endl;
     cin >> x1 >> y1;
@@ -21,9 +21,10 @@ int main(){
     cout << "Enter radius: " << endl;
     cin >> r2;
 
-    distanse = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    distance = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    sum_radius = r1 + r2;
 
-    if((r1 + r2) == distance){
+    if(sum_radius >= distance){
         cout << "Circles intersect" << endl;
     }
     else{
