d1ba309bbaf005d527a20d89f96802ce8274eb0e
user <rena040500@gmail.com>
2019-10-05 18:29:01 +0300

    Fix okn1.

--- okn1.cpp
+++ okn1.cpp
@@ -4,26 +4,38 @@
 using namespace std;
 
 int funcInt(float a, float b, float c, int x){
-    if ((x < 0) && (b != 0))
+    if ((x < 0) && (b != 0)){
+        cout << "I1" << endl;
         return (a - (x / (10 + b)));
-    if ((x > 0) && (b == 0))
+    }
+    if ((x > 0) && (b == 0)){
+        cout << "I2" << endl;
         return ((x - a) / (x - c));
-    else
+    }
+    else{
+        cout << "I3" << endl;
         return (3 * x + (2 / c));
+    }
 
 }
 
 double funcFloat(float a, float b, float c, float x){
-    if ((x < 0) && (b != 0))
+    if ((x < 0) && (b != 0)){
+        cout << "F1" << endl;
         return (a - (x / (10 + b)));
-    if ((x > 0) && (b == 0))
+    }
+    if ((x > 0) && (b == 0)){
+        cout << "F2" << endl;
         return ((x - a) / (x - c));
-    else
+    }
+    else{
+        cout << "F3" << endl;
         return (3 * x + (2 / c));
+    }
 }
 
 int main(){
-    float a, b, c, x, x1, x2, x_step, result;
+    double a, b, c, x, x1, x2, x_step, result;
 
     cout << "Enter a, b, c: " << endl;
     cin >> a;
@@ -37,12 +49,12 @@ int main(){
     cout << "Enter step: " << endl;
     cin >> x_step;
 
-    for(x_step; x1 > x2; x1 += x_step){
-        if ((((static_cast<int>(a)) == true) || ((static_cast<int>(b)) == true)) && ((static_cast<int>(c)) == true)){
-            cout << "Result: " << funcInt(a, b, c, x) << endl;
+    for(x_step; x1 < x2; x1 += x_step){
+        if (((a - static_cast<int>(a) == 0) || (b - static_cast<int>(b) == 0)) && (c - static_cast<int>(c) == 0)){
+            cout << "Result: " << funcInt(a, b, c, x + x_step) << endl;
         }
         else{
-            cout << "Resutl: " << funcFloat(a, b, c, x) << endl;
+            cout << "Resutl: " << funcFloat(a, b, c, x + x_step) << endl;
         }
     }
 
