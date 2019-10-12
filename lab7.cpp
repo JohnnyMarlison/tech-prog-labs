304a75bbcdbe1203072ad95d64cf406a31e21858
JonnhyMarlison <renat.enokaev@mail.ru>
2019-10-12 08:36:42 +0300

    Fix lab7.

--- lab7.cpp
+++ lab7.cpp
@@ -7,13 +7,17 @@ using std::endl;
 
 int main(){
 	cout << "Start program ... " << endl;
-	
-	for(int iter2 = (M_PI / 12); -M_PI < iter2; --iter2)
-		cout << sin(iter2) * sin(iter2) << endl;
-	for(int iter = (M_PI / 12); iter < M_PI; ++iter)	
-		cout << sin(iter) * sin(iter) << endl;
-	
+	float iter = -M_PI;
+	float iter2 = M_PI;
 
+	for(iter; iter <= 0; iter += (M_PI / 12)){
+		cout << sin(iter) * sin(iter) << endl;
+		cout << iter << endl;
+	}
+	for(iter2; iter2 <= M_PI ; iter2 += (M_PI / 12)){
+		cout << sin(iter2) * sin(iter2) << endl;
+		cout << iter2 << endl;
+	}
 	cout << "Program finished. " << endl;
 	return 0;
 }
