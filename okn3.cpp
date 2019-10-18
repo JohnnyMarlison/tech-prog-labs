e6d51fce63128ac28db5a1c3aa5fe4a5c0daaa60
user <rena040500@gmail.com>
2019-10-18 14:56:46 +0300

    Begin fix okn3.

--- okn3.cpp
+++ okn3.cpp
@@ -7,22 +7,23 @@
 using std::cout;
 using std::cin;
 using std::setw;
-/*
+
+/*function for enter matrix elements*/
 void enterMatrix(int arr[][size], int n){
     cout << "\n";
-    int arr[n][n];
     for(int iter = 0; iter < n; ++iter){
         for(int iter2 = 0; iter2 < n; ++iter2){
             cin >> arr[iter][iter2];
             cout << setw(4) << arr[iter][iter2] ;
-            cout << "  ";
-            cout << iter << iter2;
+            // cout << "  ";
+            // cout << iter << iter2;
         }
         cout << "\n";
     }
     cout << "\n";
 }
-*/
+
+/*function generate matrix with rand*/
 void generateMatrix(int arr[][size], int n){
     cout << "\n";
     srand(time(0));
@@ -38,35 +39,41 @@ void generateMatrix(int arr[][size], int n){
     cout << "\n";
 }
 
-int uSumColumns(int arr[][size], int n){
-    int sum = 0;
+/*function sum for columns*/
+int uSumColumns(int arr[][size], int n, int sum){
     for(int iter = 0; iter < n; ++iter){
         for(int iter2 = 0; iter2 < n; ++iter2){
-            if(arr[iter2][iter] < 0 iter ){
-                //sum = 0;
-                ++iter2;
-            }
-            else if(arr[iter2][iter] >= 0 && arr[iter2 + 1][iter] >= 0)
-                sum += arr[iter2][iter];
-            else sum = 0;
+            sum += arr[iter2][iter];
         }
     }    
+    if(sum == abs(sum))
+        return sum;
+    else 
+        return 0;
 
-    return sum;
+    //return sum;
 }
 
-int main() {
+
+int main(){
     cout << "Start program ...\n";
     
     int arr[size][size];
-    int n;
+    int n, select, sum;
+
+    cout << "Select matrix input method: \n\n";
+    cout << "1 - Enter matrix \n";
+    cout << "2 - Generate matrix \n";
+    cin >> select;
     cout << "Enter size matrix N x N: ";
     cin >> n;
 
-    generateMatrix(arr, n);
-    //enterMatrix(arr, n);
-    cout << "Sum Columns = " << uSumColumns(arr, n) << "\n";
+    if (select == 1)
+        enterMatrix(arr, n);
+    if (select == 2)
+        generateMatrix(arr, n);
 
+    cout << "Sum Columns = " << uSumColumns(arr, n, sum) << "\n";
     cout << "Program finished.\n";
     return 0;
 }
