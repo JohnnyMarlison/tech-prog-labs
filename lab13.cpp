69acb434e34163692a7e1daf599d6f265d61d260
JonnhyMarlison <renat.enokaev@mail.ru>
2019-11-01 18:01:18 +0300

    Complete lab13 and small fix.

--- lab13.cpp
+++ lab13.cpp
@@ -6,7 +6,7 @@ using std::cin;
 /*function for translate nums from any nums system to decimal*/
 unsigned long long int decimalNS(unsigned long long int num, unsigned int ns){
     unsigned long long int S,P;
-    
+
     for (S = 0, P = 1; num!= 0; S += num % 10 * P, P *= ns, num /= 10);
         return S;
 }
@@ -14,30 +14,27 @@ unsigned long long int decimalNS(unsigned long long int num, unsigned int ns){
 /*function for translate nums from decimal system to any nums system*/
 unsigned long long int anyNS(unsigned long long int num, unsigned int ns){
     unsigned long long int S,P;
-    
+
     for (S = 0, P = 1; num!= 0; S += num % ns * P, P *= 10, num /= ns);
         return S;
 }
 
 int main(){
     /*A = p, B = q, C = r, sum = A * A + A * B*/
-    unsigned long long int A, B, C; 
+    unsigned long long int A, B, C;
     unsigned int ns_A, ns_B, ns_C /*= 7*/;
 
     /*Input*/
     cout << "Enter A, num system A:\n"; cin >> A >> ns_A;
     cout << "Enter B, num system B:\n"; cin >> B >> ns_B;
     cout << "Enter num system C:\n"; cin >> ns_C;
-
-    decimalNS(A, ns_A);
-    decimalNS(B, ns_B);
 /*
-    cout << A << "(6)\n";
-    cout << B << "(4)\n";
+    cout << decimalNS(A, ns_A) << "\n";
+    cout << decimalNS(B, ns_B) << "\n";
 */
-    C = A * A + A * B;
+    C = decimalNS(A, ns_A) * decimalNS(A, ns_A) + decimalNS(A, ns_A) * decimalNS(B, ns_B);
 
-    cout << "decimal C = " << decimalNS(C, 10) << "\n";
+    cout << "decimal C = " << C << "\n";
     cout << "num system C = " << anyNS(C, ns_C) << "\n";
 
     return 0;
