e16b5f69031e2f5367c3b509324006e74346f81c
JonnhyMarlison <renat.enokaev@mail.ru>
2019-11-11 19:42:04 +0300

    Init okn4.

--- lab14.cpp
+++ lab14.cpp
@@ -7,20 +7,20 @@ using std::cin;
 using std::endl;
 
 int firstGroup(){
-    int i = 1, nums, x_st, kgr = 0, k = 1, max1, max2, konm1, konm2;
+    int i = 1, nums, x_st, count_gr = 0, k = 1, max1, max2, konm1, konm2;
     for(cout << "x = ", cin >> nums; nums != 0; i++, cin >> nums){
         if (i != 1){
             if (nums > x_st)
                 k++;
             else
                 if (k == 1){
-                    kgr++;
-                    if (kgr == 1){
+                    count_gr++;
+                    if (count_gr == 1){
                         max1 = k;
                         konm1 = i - 1;
                     }
                     else
-                        if(kgr == 2)
+                        if(count_gr == 2)
                             if (k <= max1) {
                                 max2 = k;
                                 konm2 = i - 1;
@@ -50,13 +50,13 @@ int firstGroup(){
     }
     //После цикла
     if (k > 1){
-        kgr++;
-        if (kgr == 1){
+        count_gr++;
+        if (count_gr == 1){
             max1 = k;
             konm1 = i;
         }
         else
-            if(kgr == 2)
+            if(count_gr == 2)
                 if (k <= max1) {
                     max2 = k;konm2 = i;}
                 else {
@@ -71,17 +71,17 @@ int firstGroup(){
                     max1 = k;
                     konm1 = i;
                 }
-                else
                     if (k > max2) {
+                else
                         max2 = k;
                         konm2 = i;
                     }
     }
-    // Вывод результатов
-    if (kgr == 0)
+    /*output*/
+    if (count_gr == 0)
         cout << "No groups of ascending elements\n";
     else
-        if(kgr == 1)
+        if(count_gr == 1)
             cout << "There is one group, the number of elements in it " << max1 << ". Last element - "<< konm1 << endl;
         else{
             cout << "The number of elements in the first group - " << max1 << ". Last element - " << konm1 << "." << endl;
