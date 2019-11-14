50a8ec8c67a8a2122254014bdc5e8edd65b3c323
user <rena040500@gmail.com>
2019-11-14 22:25:32 +0300

    Small fix.

--- lab_14.cpp
+++ lab_14.cpp
@@ -1,3 +1,5 @@
+/*Вводится последовательность целых чисел, найти первую и последнюю группы,
+состоящих из возрастающей последовательности цифр.*/
 #include <iostream>
 
 using std::cout;
@@ -15,18 +17,23 @@ void groups(){
     cout << "Enter nums:\n";
     for(cin >> nums; nums != 0; iter++, cin >> nums){
         if(iter != 1){
-            //nums_last = nums;
             if(!flag_first_found){  
                 if(!flag_group && nums_last < nums) {
                     indb_1 = iter - 1;
+                    //indb_2 = iter - 1;
                     flag_group = 1;
                 }
                 if(flag_group && nums_last > nums){
                     inde_1 = iter - 1;
+                    //inde_2 = iter - 1;
                     flag_group = 0;
                     if(inde_1 - indb_1 > 0){
                         flag_first_found = 1;
                     }
+                    if(indb_2 - inde_2 > 0 && flag_group ){
+                        inde_2 = iter - 1;
+                    }
+                    
                 }
             }
         }
@@ -34,6 +41,12 @@ void groups(){
     }
     if (flag_first_found == 1)
         cout << "Find first group " << inde_1 << "\n";
+    if(inde_1 - indb_1 > 0)
+        cout << "Find last group " << inde_2 << "\n";
+    else{
+        cout << "group not found\n";   
+    }
+    
 }
 
 
