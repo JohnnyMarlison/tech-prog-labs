dd4f381e5452eb7bb407df0ceae86712777f78c4
user <rena040500@gmail.com>
2019-11-15 13:13:22 +0300

    Finished lab_14.

--- lab_14.cpp
+++ lab_14.cpp
@@ -9,7 +9,7 @@ using std::cin;
 void groups(){
     int indb_1 = 0, inde_1 = 0,
         indb_2 = 0, inde_2 = 0,
-        flag_group, flag_first_found;
+        flag_group = 0, flag_first_found = 0;
 
     int iter = 1, nums, nums_last = 0, 
         count_gr = 0, count = 1;
@@ -22,31 +22,39 @@ void groups(){
                     indb_1 = iter - 1;
                     flag_group = 1;
                 }
-                if(flag_group && nums_last > nums){
+                if(flag_group && nums_last >= nums){
                     inde_1 = iter - 1;
                     flag_group = 0;
-                    if(inde_1 - indb_1 > 0){
-                        flag_first_found = 1; 
-                    }
+                    flag_first_found = 1;
                 }
+            } 
+            else {
+                if(!flag_group && nums_last < nums){
+                    indb_2 = iter - 1;
+                    flag_group = 1;
                 }
-                else{
-                    if(!flag_group && nums_last < nums){
-                        indb_2 = iter - 1;
-                        flag_group = 1; 
-                    }
-                    if(flag_group && nums < nums_last){
-                        inde_2 = iter - 1;
-                        //flag_group = 0;
-                    } 
+                if(flag_group && nums_last >= nums){
+                    inde_2 = iter - 1;
+                    flag_group = 0;
+
                 }
+            }    
         }
         nums_last = nums;    
     }
-    if (flag_first_found == 1)
-        cout << "Find first group " << inde_1 << "\n";
-    if(flag_group)
-        cout << "Find last group " << inde_2 << "\n";
+
+    if(flag_group && flag_first_found){
+        inde_2 = iter - 1;
+    }
+    if(flag_group && !flag_first_found){
+        inde_1 = iter - 1;
+    }
+
+
+    if (indb_1)
+        cout << "Find first group index: " << indb_1 << " " << inde_1 << "\n";
+    if (indb_2)
+        cout << "Find last group index: " << indb_2 << " " << inde_2 << "\n";
     else{
         cout << "group not found\n";   
     }
