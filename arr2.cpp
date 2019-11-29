cdc93d08bf2cca3e7ff5b8fbaf2cdd0803176e76
JonnhyMarlison <renat.enokaev@mail.ru>
2019-11-29 16:57:40 +0300

    add arr2.

--- arr2.cpp
+++ arr2.cpp
@@ -41,7 +41,8 @@ void generateArray(int arr1[], int N, int first, int last){
     cout << '\n';
 }
 
-void sixMax(int arr1[], int N, int max[],int M){
+/*fucntion find six max element*/
+void sixMax(int arr1[], int N, int max[], int M){
     int max1 = -INT_MAX;
     int iter2 = 0;
     
@@ -49,8 +50,6 @@ void sixMax(int arr1[], int N, int max[],int M){
         for(int iter = 0; iter < N; ++iter){
             if(arr1[iter] > max1){
                 max1 = arr1[iter];
-                max[iter2] = arr1[iter];
-                iter2 += 1; 
             }
         }
     }
